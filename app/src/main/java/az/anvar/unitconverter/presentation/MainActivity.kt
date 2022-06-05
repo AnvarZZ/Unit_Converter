9307992944ca853a14a76c69630c80f20ae968e3
AnvarZZ <anvarzeynalzade@gmail.com>
2022-06-05 21:08:25 +0400

    Main commit

---
+++ app/src/main/java/az/anvar/unitconverter/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package az.anvar.unitconverter.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import az.anvar.unitconverter.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
